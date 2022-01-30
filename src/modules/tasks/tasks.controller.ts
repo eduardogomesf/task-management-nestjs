156ea0014fc042ab91fcdfde33482a0d70d75d26
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-29 21:11:04 -0300

    feat: Delete task endpoint

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
 
@@ -19,5 +19,10 @@ export class TasksController {
   @Get('/:id')
   getTaskById(@Param('id') id: string) {
     return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    return this.tasksService.deleteTask(id);
   }
 }
