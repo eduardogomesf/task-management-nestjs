cc83f09ff441c81180d24bd84ccdb5615bcba999
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-30 19:36:00 -0300

    feat: Creating Sign In endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
   async signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signIn')
+  async signIn(@Body() authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayloadInterface } from './interface/jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -10,6 +12,7 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
@@ -19,5 +22,22 @@ export class AuthService {
     const passwordHash = await bcrypt.hash(password, salt);
 
     return this.userRepository.createUser({ username, password: passwordHash });
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<JwtPayloadInterface> {
+    const { password, username } = authCredentialsDto;
+
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { username };
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check your sign-in credentials');
+    }
   }
 }
