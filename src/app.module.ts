7a1e24f00894dc0b1d8faa31bc1f2396854a2e6e
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-02-05 21:52:53 -0300

    Adding config for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     AuthModule,
     TypeOrmModule.forRoot({
